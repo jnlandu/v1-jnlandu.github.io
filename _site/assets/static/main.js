334d81d1033cdbf88e177dda8cc72eac643b3264
= <=>
2024-08-18 17:59:14 +0200

    Update layouts from previous personal website

---
+++ _site/assets/static/main.js
@@ -0,0 +1,96 @@
+// const navLinks = document.querySelectorAll(".nav-link")
+// for (i=0;i< navLinks.length; i++){
+//     navLinks[i].addEventListener('click', 
+//         function(){
+//             navLinks[i].style.color="#ff6a81"
+//             navLinks[i].style.filter="contrast(1)"
+//         }
+// )}
+
+
+
+
+// document.querySelectorAll('.nav-link').forEach(link => {
+//     link.addEventListener('click', function() {
+//         // Remove 'active' class from all links
+//         document.querySelectorAll('.nav-link').forEach(navLink => {
+//             navLink.classList.remove('active');
+//         });
+//         // Add 'active' class to the clicked link
+//         this.classList.add('active');
+//     });
+// });
+
+
+
+
+  //   const navLinks = document.querySelectorAll('.nav-link')
+  //   navLinks.forEach(
+  //     link => link.addEventListener(
+  //         'click',
+  //         function(){
+  //             link.style.color="red"
+  //             link.classList.add("active")
+  //         }
+         
+  //     )
+// )
+// const links = document.querySelectorAll('.clicked');
+// let i = links.length - 1; // Start from the last index
+
+// do {
+//     links[i].style.color = "white";
+//     links[i].classList.add('clicked=false')
+//     i = i - 1;
+// } while (i >= 1)
+// links[0].style.color ="red"
+// links[0].classList.add('active')
+
+// links[1].addEventListener('click',
+//   function(){
+//     links[0].classList.remove('active')
+//     // this.style.color="red"
+//     // this.classList.add('active')
+//   }
+// )
+
+
+
+// links.forEach(
+//   link =>{
+//     link.addEventListener(
+//       'click',
+//       function(){
+//         if (link.innerText === 'home'){
+//           link.style.color ="red"
+//           link.classList.add('active')
+//           link.classList.add('clicked=True')
+
+//         }
+//         link.classList.add('clicked=false')
+//       } 
+//     )
+// }
+// )
+
+  
+//   // links[0].style.color ="#ff6a81"
+//   // links[0].style.filer="constrast(1)"
+//   // links[0].classList.add('active')
+
+//   // for ( i=0; i< links.length; i++){
+//   //   links[i].addEventListener(
+//   //     'click',
+//   //     function(){
+//   //       for ( i=0; i< links.length; i++){
+//   //         links[i].classList.remove('active')
+//   //       }
+//   //       links[i].style.color ="red"
+//   //     }
+//   //   )
+//   //   links[i].classList.add('active')
+//   // }
+  
+
+
+
